d5143be0b515a01b27946e41e22837174b801281
Fernando <fgpaolini@gmail.com>
2025-10-07 18:46:48 +0200

    - Modificacion de Grid styling. - Cambio a loops en el renderizado de teclas y cajas.

--- src/wordle.tsx
+++ src/wordle.tsx
@@ -1,28 +1,18 @@
 import './wordle.css'
 
+const WordBox = ({ key }: { key: string }) => {
+    return <div className="grid-item"><h1>{key}</h1></div>;
+}
 
 const Grid = () => {
-    // Create an array with 10 elements
-
     return (
         <div className="grid-container">
-            {Array.from({ length: 5 }).map((_, rowIndex) => (
-                <div className="grid-item"> <h1>A</h1> </div>
-            ))}
-            {Array.from({ length: 5 }).map((_, rowIndex) => (
-                <div className="grid-item"> </div>
-            ))}
-            {Array.from({ length: 5 }).map((_, rowIndex) => (
-                <div className="grid-item"> </div>
-            ))}
-            {Array.from({ length: 5 }).map((_, rowIndex) => (
-                <div className="grid-item"> </div>
-            ))}
-            {Array.from({ length: 5 }).map((_, rowIndex) => (
-                <div className="grid-item"> </div>
-            ))}
-            {Array.from({ length: 5 }).map((_, rowIndex) => (
-                <div className="grid-item"></div>
+            {Array.from({ length: 6 }).map((_, rowIndex) => (
+                <div key={rowIndex} className="grid-row">
+                    {Array.from({ length: 5 }).map((_, colIndex) => (
+                        <WordBox key={`${rowIndex}-${colIndex}`} />
+                    ))}
+                </div>
             ))}
         </div>
     );
@@ -30,36 +20,37 @@ const Grid = () => {
 
 
 const Keyboard = () => {
-    const keys_1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
-    const keys_2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
-    const keys_3 = ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'DEL'];
+    const KEYS = [
+        ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+        ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+        ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'DEL']
+    ];
+
+
+    const handleKeyPress = (key: string) => {
+        console.log(key);
+    };
 
     return (
         <div className="keyboard-container">
-            <div className="keyboard-row keyboard-row-1">
-                {keys_1.map((key) => (
-                    <button key={key} className="key">{key}</button>
-                ))}
-            </div>
-            <div className="keyboard-row  keyboard-row-2">
-                {keys_2.map((key) => (
-                    <button key={key} className="key">{key}</button>
-                ))}
-            </div>
-            <div className="keyboard-row keyboard-row-3">
-                {keys_3.map((key) =>
-                    <button key={key} className={`${key.toLowerCase()} key`}>{key}</button>
-                )}
-            </div>
+            {KEYS.map((row, rowIndex) => (
+                <div key={rowIndex} className={`keyboard-row keyboard-row-${rowIndex+1}`}>
+                    {row.map((key) => (
+                        <button key={key} className={`${key.toLowerCase()} key`} onClick={() => handleKeyPress(key)}>{key}</button>
+                        ))}
+                </div>
+            ))}
         </div>
     )
-
 }
 
 const Wordle = () => {
 
+    const PALABRA_DEL_DIA = "PERRO";
+
     return (
         <div className="wordle-container">
+            <h1>{PALABRA_DEL_DIA}</h1>
             <Grid />
             <Keyboard />
         </div>
